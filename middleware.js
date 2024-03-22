2e9f20fa80e8514497d93c318f5f0e968cd7feb9
Rajat Panwar <ablognet@gmail.com>
2024-03-22 22:54:39 +0530

    feat. check if user is authenticated or not

---
+++ middleware.js
@@ -0,0 +1,34 @@
+import { getCurrentSession } from "@/lib/supabase/actions/server";
+import { NextResponse } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request) {
+  const { session } = await getCurrentSession();
+  const { pathname } = request.nextUrl;
+
+  // Check if the user is authenticated and trying to access a protected path
+  if (pathname.startsWith(`/dashboard`) && !session) {
+    // Redirect the user to the /auth page if not authenticated
+    return NextResponse.redirect(new URL(`/auth`, request.url));
+  }
+
+  // If the user is not authenticated and trying to access the /auth page
+  if (pathname.startsWith(`/auth`) && session) {
+    // Redirect the user to the /dashboard page if authenticated
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  // If the user is authenticated and trying to access any other path
+  if (session) {
+    // Allow access to the requested path
+    return NextResponse.next();
+  }
+
+  // For all other cases, allow the request to continue
+  // return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/dashboard/:path*", "/auth"],
+};

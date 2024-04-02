437c96215a70b21caa7a5d1818c13f7a3c37d828
Rajat Panwar <ablognet@gmail.com>
2024-04-02 22:33:01 +0530

    fix: supabase module not found

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -1,21 +1,21 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import createSupabaseServerClient from "@/lib/supabase/server";
+// import createSupabaseServerClient from "@/lib/supabase/server";
 
 export async function GET(request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the Auth Helpers package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
   const requestUrl = new URL(request.url);
-  const code = requestUrl.searchParams.get("code");
-  console.log("this has been called", code);
+  // const code = requestUrl.searchParams.get("code");
+  // console.log("this has been called", code);
 
-  if (code) {
-    const cookieStore = cookies();
-    const supabase = createSupabaseServerClient(cookieStore);
-    await supabase.auth.exchangeCodeForSession(code);
-  }
+  // if (code) {
+  //   const cookieStore = cookies();
+  //   const supabase = createSupabaseServerClient(cookieStore);
+  //   await supabase.auth.exchangeCodeForSession(code);
+  // }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(new URL("/x", request.url));
+  return NextResponse.redirect(new URL("/auth", request.url));
 }

91df8a1dcafd45e4a63fa79e839613a281c4f17c
Rajat Panwar <ablognet@gmail.com>
2024-09-20 09:07:48 +0530

    removed flux beta

---
+++ app/dashboard/flux/page.jsx
@@ -0,0 +1,18 @@
+export default function Flux() {
+  return (
+    <>
+      <p>
+        The new flux based pipeline is out of beta and available for all users.
+        The new flux based model is capable of generating ultra-realistic
+        headshots.
+      </p>
+      <br></br>
+      <a
+        href="/dashboard/studio/create"
+        className="text-blue-600 hover:underline"
+      >
+        Click here to generate your AI headshots.
+      </a>
+    </>
+  );
+}

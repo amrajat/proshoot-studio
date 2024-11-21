07a2b983d094ec4584730d27a2b4f600e8ea51c1
Rajat Panwar <ablognet@gmail.com>
2024-11-21 19:56:35 +0530

    chore: add padding to paragraph

--- components/homepage/Examples.jsx
+++ components/homepage/Examples.jsx
@@ -8,20 +8,21 @@ function Examples() {
       <div className="flex justify-center mb-4">
         <StarRatings size="size-4" />
       </div>
-
-      <SectionParaHeading
-        badgeText={"Trust"}
-        title={"Reviews, Examples, and Ratings"}
-      >
-        Professional online profiles are drastically changing with our Proshoot
-        AI Headshot Generator. Customers enjoy the simplicity of the process,
-        high-quality images, and their ethnicity being represented accurately.
-        They appreciate the strong resemblance to their features and the wide
-        range of style options available. Plus, our technology ensures no
-        deformation, guaranteeing a natural and authentic look. Measures are
-        actively being put in place to improve on generating the same at a
-        faster rate.
-      </SectionParaHeading>
+      <div className="container mx-auto px-6 sm:px-6 lg:px-8">
+        <SectionParaHeading
+          badgeText={"Trust"}
+          title={"Reviews, Examples, and Ratings"}
+        >
+          Professional online profiles are drastically changing with our
+          Proshoot AI Headshot Generator. Customers enjoy the simplicity of the
+          process, high-quality images, and their ethnicity being represented
+          accurately. They appreciate the strong resemblance to their features
+          and the wide range of style options available. Plus, our technology
+          ensures no deformation, guaranteeing a natural and authentic look.
+          Measures are actively being put in place to improve on generating the
+          same at a faster rate.
+        </SectionParaHeading>
+      </div>
       <Reviews />
     </section>
   );

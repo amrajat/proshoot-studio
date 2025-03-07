c0ab3d7b99a3820bd6ed9266ca632a49e1db83b7
Rajat Panwar <ablognet@gmail.com>
2025-03-07 23:16:19 +0530

    disabled temp. Intercom

--- components/IntercomMessenger.jsx
+++ components/IntercomMessenger.jsx
@@ -1,51 +1,51 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import Intercom from "@intercom/messenger-js-sdk";
-import createSupabaseBrowserClient from "@/lib/supabase/BrowserClient";
+// import React, { useEffect, useState } from "react";
+// import Intercom from "@intercom/messenger-js-sdk";
+// import createSupabaseBrowserClient from "@/lib/supabase/BrowserClient";
 
 // Initialize Supabase client
-const supabase = createSupabaseBrowserClient();
+// const supabase = createSupabaseBrowserClient();
 
 export default function IntercomMessenger() {
-  const [loading, setLoading] = useState(true);
-  const [errorMessage, setErrorMessage] = useState("");
+  // const [loading, setLoading] = useState(true);
+  // const [errorMessage, setErrorMessage] = useState("");
 
-  useEffect(() => {
-    const fetchUserSession = async () => {
-      try {
-        const {
-          data: { session },
-          error,
-        } = await supabase.auth.getSession();
+  // useEffect(() => {
+  //   const fetchUserSession = async () => {
+  //     try {
+  //       const {
+  //         data: { session },
+  //         error,
+  //       } = await supabase.auth.getSession();
 
-        if (error) {
-          throw new Error("Error fetching session: " + error.message);
-        }
+  //       if (error) {
+  //         throw new Error("Error fetching session: " + error.message);
+  //       }
 
-        if (session) {
-          const { user } = session;
-          Intercom({
-            app_id: "jvwzr8qk",
-            user_id: user.id,
-            name: user.raw_user_meta_data?.full_name || "there!",
-            email: user.email,
-            created_at: user.created_at,
-          });
-        } else {
-          Intercom({
-            app_id: "jvwzr8qk",
-          });
-        }
-      } catch (error) {
-        setErrorMessage(error.message);
-        console.error("Intercom initialization error:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  //       if (session) {
+  //         const { user } = session;
+  //         Intercom({
+  //           app_id: "jvwzr8qk",
+  //           user_id: user.id,
+  //           name: user?.raw_user_meta_data?.full_name || "there!",
+  //           email: user?.email,
+  //           created_at: user?.created_at,
+  //         });
+  //       } else {
+  //         Intercom({
+  //           app_id: "jvwzr8qk",
+  //         });
+  //       }
+  //     } catch (error) {
+  //       setErrorMessage(error.message);
+  //       console.error("Intercom initialization error:", error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    fetchUserSession();
-  }, []);
+  //   fetchUserSession();
+  // }, []);
 
-  return null; // Return null if everything is fine
+  return null;
 }

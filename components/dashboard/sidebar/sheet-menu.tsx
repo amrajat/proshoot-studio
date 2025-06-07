1c506071c66d667308f8ebf7b15c1a58faa5db56
Rajat Panwar <ablognet@gmail.com>
2025-06-07 21:41:40 +0530

    fix: sheet menu

--- components/dashboard/sidebar/sheet-menu.tsx
+++ components/dashboard/sidebar/sheet-menu.tsx
@@ -1,26 +1,14 @@
-import Link from "next/link";
-import {
-  AudioWaveform,
-  GalleryVerticalEnd,
-  MenuIcon,
-  PanelsTopLeft,
-} from "lucide-react";
+import { MenuIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Menu } from "@/components/dashboard/sidebar/menu";
-import {
-  Sheet,
-  SheetHeader,
-  SheetContent,
-  SheetTrigger,
-  SheetTitle,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { AccountSwitcher } from "./account-switcher";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 export function SheetMenu() {
   return (
-    <Sheet>
+    <Sheet modal={false}>
       <SheetTrigger className="lg:hidden" asChild>
         <Button className="h-8" variant="outline" size="icon">
           <MenuIcon size={20} />
@@ -28,20 +16,12 @@ export function SheetMenu() {
       </SheetTrigger>
       <SheetContent className="sm:w-72 px-3 h-full flex flex-col" side="left">
         <SidebarProvider>
-          <SheetHeader>
-            {/* <Button
-            className="flex justify-center items-center pb-2 pt-1"
-            variant="link"
-            asChild
-          >
-            <Link href="/dashboard" className="flex items-center gap-2">
-              <PanelsTopLeft className="w-6 h-6 mr-1" />
-              <SheetTitle className="font-bold text-lg">Brand</SheetTitle>
-            </Link>
-          </Button> */}
+          <div className="flex-shrink-0 mt-2 text-primary underline-offset-4 hover:underline">
             <AccountSwitcher />
-          </SheetHeader>
-          <Menu isOpen />
+          </div>
+          <div className="flex-1 overflow-y-auto">
+            <Menu isOpen />
+          </div>
         </SidebarProvider>
       </SheetContent>
     </Sheet>
